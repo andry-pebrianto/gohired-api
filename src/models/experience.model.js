d22cf7056be6863a68162fb7352cd3083f95435e
Andry Pebrianto <andrypeb227@gmail.com>
2022-05-31 01:13:43 +0700

    Fix bug update experience

--- src/models/experience.model.js
+++ src/models/experience.model.js
@@ -27,12 +27,12 @@ module.exports = {
   }),
   addExperience: (data) => new Promise((resolve, reject) => {
     const {
-      id, position, photo, company, startDate, endDate, description, userId, createdAt,
+      id, position, photo, company, start_date, end_date, description, userId, createdAt,
     } = data;
 
     db.query(
       'INSERT INTO experiences (id, position, photo, company, start_date, end_date, description, user_id, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-      [id, position, photo, company, startDate, endDate, description, userId, createdAt],
+      [id, position, photo, company, start_date || null, end_date || null, description, userId, createdAt],
       (error, result) => {
         if (error) {
           reject(error);
